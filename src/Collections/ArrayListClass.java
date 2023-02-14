e1c249321510951b9c3481f4474c00a640840c3e
kulkarni42vivek <kulkarni42vivek@neml.in>
2023-02-15 01:58:13 +0530

    INITIAL COMMIT

---
+++ src/Collections/ArrayListClass.java
@@ -0,0 +1,12 @@
+package Collections;
+
+import java.util.*;
+
+public class ArrayListClass {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("VALUE");
+        list.add("VALUE");
+
+    }
+}
