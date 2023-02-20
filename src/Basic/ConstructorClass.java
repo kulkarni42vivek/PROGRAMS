8c7ffd1b19f7ca4fb597249af6bc3172ef78eccd
kulkarni42vivek <kulkarni42vivek@neml.in>
2023-02-20 18:51:57 +0530

    changes

---
+++ src/Basic/ConstructorClass.java
@@ -0,0 +1,12 @@
+package Basic;
+
+class Student1{
+    int id ;
+    String name ;
+    
+}
+public class ConstructorClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Basic/OopsClass.java
@@ -0,0 +1,75 @@
+package Basic;
+
+// main concepts
+// class , objects , inheritance , polymorphism , encapsulation , abstraction
+// extra concepts - coupling , cohesion , association , aggreagaaiton , composition
+
+import java.rmi.StubNotFoundException;
+import java.security.spec.ECField;
+
+interface testInterface {
+    public void testMethodI();
+}
+class Student {
+    int id ;
+    String name ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+// different ways to initialize the object s
+// using constructor , using method , using reference variable
+// objects gets stored in the heap memory while its reference in the stack
+
+
+
+// different ways to initialize the object
+// new keyword
+// new instance method
+// clone method
+// deserialization
+// by factory methods
+
+public class OopsClass {
+    public static void main(String[] args)    {
+        Student st = new Student(1,"one");
+        System.out.println(st);
+
+        try {
+            Class c = Class.forName("Student");
+            Student st1 =(Student) c.newInstance();
+            System.out.println(st1);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
