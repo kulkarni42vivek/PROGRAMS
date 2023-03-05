208542f4c2fc70a8061cb6193bd95deca01f5ff0
kulkarni42vivek <kulkarni42vivek@neml.in>
2023-03-05 15:38:28 +0530

    changes

--- PepCoding/src/Patterns/Pattern8.java
+++ PepCoding/src/Patterns/Pattern8.java
@@ -2,13 +2,13 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Pattern7{
+public class Pattern8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i =1;i<=n;i++){
-            for(int j =1;j<=n;j++){
-                if(i == j) {
+        for(int i =0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if(i+j == n-1){
                     System.out.print("*");
                 }
                 else{
